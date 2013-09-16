f242806bf00e0f0f4456c0fde20daf5ae86547c7
markkeffer <mark.v.keffer@gmail.com>
2013-09-16 10:26:01 -0400

    Update Grid.java
    Increasing to 10x10 grid default

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Grid.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Grid.java
@@ -6,8 +6,8 @@ import static com.wakaleo.gameoflife.domain.Cell.LIVE_CELL;
 // Adding a comment for my jenkins testing
 public class Grid {
 
-    private static final int DEFAULT_ROW_COUNT = 3;
-    private static final int DEFAULT_COLUMN_COUNT = 3;
+    private static final int DEFAULT_ROW_COUNT = 10;
+    private static final int DEFAULT_COLUMN_COUNT = 10;
 
     private Cell[][] cells;
 
